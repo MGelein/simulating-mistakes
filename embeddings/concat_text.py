2ccd4a8abe596ac2867fa6b582e37d993d8ead61
Mees Gelein <meesgelein@gmail.com>
2021-01-07 16:37:47 +0100

    All words have been concatenated

--- embeddings/concat_text.py
+++ embeddings/concat_text.py
@@ -1,4 +1,12 @@
 import os
+from tqdm import tqdm
 
-for file in os.listdir('./text/'):
-    print(file)
+lines = []
+for file in tqdm(os.listdir('./text/')):
+    with open('./text/' + file, 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            lines.append(line)
+
+with open('./words.txt', 'w', encoding='utf8') as f:
+    for line in tqdm(lines):
+        f.write(line)
