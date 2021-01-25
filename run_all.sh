42805a8300ba225dea0d9b0741dd03dace68e0be
Mees Gelein <meesgelein@gmail.com>
2021-01-25 12:24:19 +0100

    Creating extra tests for some more insight

--- run_all.sh
+++ run_all.sh
@@ -87,3 +87,9 @@ python main.py --settings ./runs/mem_04_09.ini
 python analyse.py -i ./runs/output/mem_04_09 -n 6
 python main.py --settings ./runs/mem_06_10.ini
 python analyse.py -i ./runs/output/mem_06_10 -n 6
+python main.py --settings ./runs/mem_00_07.ini
+python analyse.py -i ./runs/output/mem_00_07 -n 6
+python main.py --settings ./runs/mem_02_09.ini
+python analyse.py -i ./runs/output/mem_02_09 -n 6
+python main.py --settings ./runs/mem_04_10.ini
+python analyse.py -i ./runs/output/mem_04_10 -n 6
--- run_simulations.sh
+++ run_simulations.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
-python main.py --settings ./runs/mem_00_01.ini
-python analyse.py -i ./runs/output/mem_00_01 -n 6
-python main.py --settings ./runs/mem_02_03.ini
-python analyse.py -i ./runs/output/mem_02_03 -n 6
-python main.py --settings ./runs/mem_04_05.ini
-python analyse.py -i ./runs/output/mem_04_05 -n 6
-python main.py --settings ./runs/mem_06_07.ini
-python analyse.py -i ./runs/output/mem_06_07 -n 6
-python main.py --settings ./runs/mem_08_09.ini
-python analyse.py -i ./runs/output/mem_08_09 -n 6
-python main.py --settings ./runs/mem_10_10.ini
-python analyse.py -i ./runs/output/mem_10_10 -n 6
-python main.py --settings ./runs/mem_00_03.ini
-python analyse.py -i ./runs/output/mem_00_03 -n 6
-python main.py --settings ./runs/mem_02_05.ini
-python analyse.py -i ./runs/output/mem_02_05 -n 6
-python main.py --settings ./runs/mem_04_07.ini
-python analyse.py -i ./runs/output/mem_04_07 -n 6
-python main.py --settings ./runs/mem_06_09.ini
-python analyse.py -i ./runs/output/mem_06_09 -n 6
-python main.py --settings ./runs/mem_08_10.ini
-python analyse.py -i ./runs/output/mem_08_10 -n 6
-python main.py --settings ./runs/mem_00_05.ini
-python analyse.py -i ./runs/output/mem_00_05 -n 6
-python main.py --settings ./runs/mem_02_07.ini
-python analyse.py -i ./runs/output/mem_02_07 -n 6
-python main.py --settings ./runs/mem_04_09.ini
-python analyse.py -i ./runs/output/mem_04_09 -n 6
-python main.py --settings ./runs/mem_06_10.ini
-python analyse.py -i ./runs/output/mem_06_10 -n 6
+
+python main.py --settings ./runs/mem_00_07.ini
+python analyse.py -i ./runs/output/mem_00_07 -n 6
+python main.py --settings ./runs/mem_02_09.ini
+python analyse.py -i ./runs/output/mem_02_09 -n 6
+python main.py --settings ./runs/mem_04_10.ini
+python analyse.py -i ./runs/output/mem_04_10 -n 6
