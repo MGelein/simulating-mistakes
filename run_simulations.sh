deaf90dc0257ab3c1c719b9f9966526ed00b8ac9
Mees Gelein <meesgelein@gmail.com>
2021-01-25 14:09:02 +0100

    ready for arrogance run

--- run_simulations.sh
+++ run_simulations.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-python main.py --settings ./runs/voc_00_01.ini
-python analyse.py -i ./runs/output/voc_00_01 -n 6
-python main.py --settings ./runs/voc_02_03.ini
-python analyse.py -i ./runs/output/voc_02_03 -n 6
-python main.py --settings ./runs/voc_04_05.ini
-python analyse.py -i ./runs/output/voc_04_05 -n 6
-python main.py --settings ./runs/voc_06_07.ini
-python analyse.py -i ./runs/output/voc_06_07 -n 6
-python main.py --settings ./runs/voc_08_09.ini
-python analyse.py -i ./runs/output/voc_08_09 -n 6
-python main.py --settings ./runs/voc_10_10.ini
-python analyse.py -i ./runs/output/voc_10_10 -n 6
-python main.py --settings ./runs/voc_00_03.ini
-python analyse.py -i ./runs/output/voc_00_03 -n 6
-python main.py --settings ./runs/voc_02_05.ini
-python analyse.py -i ./runs/output/voc_02_05 -n 6
-python main.py --settings ./runs/voc_04_07.ini
-python analyse.py -i ./runs/output/voc_04_07 -n 6
-python main.py --settings ./runs/voc_06_09.ini
-python analyse.py -i ./runs/output/voc_06_09 -n 6
-python main.py --settings ./runs/voc_08_10.ini
-python analyse.py -i ./runs/output/voc_08_10 -n 6
-python main.py --settings ./runs/voc_00_05.ini
-python analyse.py -i ./runs/output/voc_00_05 -n 6
-python main.py --settings ./runs/voc_02_07.ini
-python analyse.py -i ./runs/output/voc_02_07 -n 6
-python main.py --settings ./runs/voc_04_09.ini
-python analyse.py -i ./runs/output/voc_04_09 -n 6
-python main.py --settings ./runs/voc_06_10.ini
-python analyse.py -i ./runs/output/voc_06_10 -n 6
-python main.py --settings ./runs/voc_00_07.ini
-python analyse.py -i ./runs/output/voc_00_07 -n 6
-python main.py --settings ./runs/voc_02_09.ini
-python analyse.py -i ./runs/output/voc_02_09 -n 6
-python main.py --settings ./runs/voc_04_10.ini
-python analyse.py -i ./runs/output/voc_04_10 -n 6
+python main.py --settings ./runs/arr_000_001.ini
+python analyse.py -i ./runs/output/arr_000_001 -n 6
+python main.py --settings ./runs/arr_020_021.ini
+python analyse.py -i ./runs/output/arr_020_021 -n 6
+python main.py --settings ./runs/arr_040_041.ini
+python analyse.py -i ./runs/output/arr_040_041 -n 6
+python main.py --settings ./runs/arr_060_061.ini
+python analyse.py -i ./runs/output/arr_060_061 -n 6
+python main.py --settings ./runs/arr_080_081.ini
+python analyse.py -i ./runs/output/arr_080_081 -n 6
+python main.py --settings ./runs/arr_100_100.ini
+python analyse.py -i ./runs/output/arr_100_100 -n 6
+python main.py --settings ./runs/arr_000_010.ini
+python analyse.py -i ./runs/output/arr_000_010 -n 6
+python main.py --settings ./runs/arr_020_030.ini
+python analyse.py -i ./runs/output/arr_020_030 -n 6
+python main.py --settings ./runs/arr_040_050.ini
+python analyse.py -i ./runs/output/arr_040_050 -n 6
+python main.py --settings ./runs/arr_060_070.ini
+python analyse.py -i ./runs/output/arr_060_070 -n 6
+python main.py --settings ./runs/arr_080_090.ini
+python analyse.py -i ./runs/output/arr_080_090 -n 6
+python main.py --settings ./runs/arr_000_030.ini
+python analyse.py -i ./runs/output/arr_000_030 -n 6
+python main.py --settings ./runs/arr_020_050.ini
+python analyse.py -i ./runs/output/arr_020_050 -n 6
+python main.py --settings ./runs/arr_040_070.ini
+python analyse.py -i ./runs/output/arr_040_070 -n 6
+python main.py --settings ./runs/arr_060_090.ini
+python analyse.py -i ./runs/output/arr_060_090 -n 6
+python main.py --settings ./runs/arr_080_100.ini
+python analyse.py -i ./runs/output/arr_080_100 -n 6
+python main.py --settings ./runs/arr_000_060.ini
+python analyse.py -i ./runs/output/arr_000_060 -n 6
+python main.py --settings ./runs/arr_020_080.ini
+python analyse.py -i ./runs/output/arr_020_080 -n 6
+python main.py --settings ./runs/arr_040_100.ini
+python analyse.py -i ./runs/output/arr_040_100 -n 6
+python main.py --settings ./runs/arr_060_100.ini
+python analyse.py -i ./runs/output/arr_060_100 -n 6
